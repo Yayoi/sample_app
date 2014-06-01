c7271ed12a91a55c73b715e457da32f8b2696295
yayoi <s1140138@gmail.com>
2014-06-01 19:42:02 +0900

    finish 7.3.2

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,7 +2,14 @@ require 'spec_helper'
 
 describe "User pages" do
   subject { page }
-  
+ 
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
+
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
@@ -11,7 +18,7 @@ describe "User pages" do
     it { should have_title(user.name) }
   end
 
-  describe "singup page" do
+  describe "signup" do
     before { visit signup_path }
 
     let(:submit) { "Create my account" }
